887092d019389c5d2da6d2612697356ca58ea32e
Wojciech Kordalski <wojtek.kordalski@gmail.com>
2016-01-15 21:57:08 +0100

    Some clean-up within Window class.

--- src/window.cc
+++ src/window.cc
@@ -1,3 +1,50 @@
 #include "window.hh"
 
-// TODO
+
+namespace tui {
+
+    Window::Window(Widget *parent, Point position, Size size) : Widget(parent), position(position), size(size) {
+      cwin = newwin(size.h, size.w, position.y, position.x);
+      window_locator = [this](Size parent_size) -> std::pair<Point, Size>
+      {
+        return std::make_pair(this->position, this->size);
+      };
+    }
+    Window::~Window() {
+      delwin(cwin);
+    }
+
+    void Window::refresh() {
+      for(auto w : widgets) {
+        w->draw();
+      }
+      ::wrefresh(cwin);
+    }
+    void Window::resize(Point position, Size size) {
+      delwin(cwin);
+      this->position = position;
+      this->size = size;
+      cwin = newwin(size.h, size.w, position.y, position.x);
+    }
+
+    void Window::add(Widget *widget) {
+      for(auto w : widgets) {
+        if(widget == w) throw std::logic_error("Can not add the same widget two times.");
+      }
+      widgets.push_back(widget);
+    }
+
+    WINDOW * Window::get_curses_window() { return cwin; }
+
+    const Window * Window::get_window() const { return this; }
+    Window * Window::get_window() { return this; }
+
+    void Window::draw() {}
+
+    void Window::parent_resize(Size parent_size) {
+      Point p;
+      Size s;
+      std::tie(p, s) = this->window_locator(parent_size);
+      this->resize(p, s);
+    }
+}
--- src/window.hh
+++ src/window.hh
@@ -11,57 +11,30 @@
 
 namespace tui {
   class Window : public Widget {
+  public:
+    Window(Widget *parent, Point position, Size size);
+    virtual ~Window();
+
+    virtual void refresh();
+    virtual void resize(Point position, Size size);
+
+    void add(Widget *widget);
+
+    WINDOW * get_curses_window();
+
+    virtual const Window * get_window() const;
+    virtual Window * get_window();
+
+    virtual void draw();
+  protected:
+    virtual void parent_resize(Size parent_size);
+
   protected:
     WINDOW *cwin = nullptr; // curses window
     Point position;
     Size size;
     Locator window_locator;
     std::vector<Widget *> widgets;
-  public:
-    Window(Widget *parent, Point position, Size size) : Widget(parent), position(position), size(size) {
-      cwin = newwin(size.h, size.w, position.y, position.x);
-      window_locator = [this](Size parent_size) -> std::pair<Point, Size>
-      {
-        return std::make_pair(this->position, this->size);
-      };
-    }
-    virtual ~Window() {
-      delwin(cwin);
-    }
-
-    virtual void refresh() {
-      for(auto w : widgets) {
-        w->draw();
-      }
-      ::wrefresh(cwin);
-    }
-    virtual void resize(Point position, Size size) {
-      delwin(cwin);
-      this->position = position;
-      this->size = size;
-      cwin = newwin(size.h, size.w, position.y, position.x);
-    }
-
-    void add(Widget *widget) {
-      for(auto w : widgets) {
-        if(widget == w) throw std::logic_error("Can not add the same widget two times.");
-      }
-      widgets.push_back(widget);
-    }
-
-    WINDOW * get_curses_window() { return cwin; }
-
-    virtual const Window * get_window() const { return this; }
-    virtual Window * get_window() { return this; }
-
-    virtual void draw() {}
-  protected:
-    virtual void parent_resize(Size parent_size) {
-      Point p;
-      Size s;
-      std::tie(p, s) = this->window_locator(parent_size);
-      this->resize(p, s);
-    }
   };
 }
 
